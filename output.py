a6b14d368fabd2a0115031b0caa10c82d54c54d8
coffeecory <cory.crook@te.com>
2022-10-18 13:24:47 -0400

    ioc-parser save

--- output.py
+++ output.py
@@ -31,7 +31,7 @@ class OutputHandler(object):
 
 class OutputHandler_csv(OutputHandler):
     def __init__(self):
-        self.csv_writer = csv.writer(sys.stdout, delimiter = '\t')
+        self.csv_writer = csv.writer(sys.stdout, delimiter = '/t')
 
     def print_match(self, fpath, page, name, match):
         self.csv_writer.writerow((fpath, page, name, match))
@@ -73,15 +73,15 @@ class OutputHandler_yara(OutputHandler):
         
         string_id = "$%s%d" % (name, self.cnt[name])
         self.sids.append(string_id)
-        string_value = match.replace('\\', '\\\\')
-        print("\t\t%s = \"%s\"" % (string_id, string_value))
+        string_value = match.replace('//', '////')
+        print("/t/t%s = /"%s/"" % (string_id, string_value))
 
     def print_header(self, fpath):
         rule_name = os.path.splitext(os.path.basename(fpath))[0].translate(self.rule_enc)
 
         print("rule %s" % (rule_name))
         print("{")
-        print("\tstrings:")
+        print("/tstrings:")
 
         self.cnt = {}
         self.sids = []
@@ -89,8 +89,8 @@ class OutputHandler_yara(OutputHandler):
     def print_footer(self, fpath):
         cond = ' or '.join(self.sids)
 
-        print("\tcondition:")
-        print("\t\t" + cond)
+        print("/tcondition:")
+        print("/t/t" + cond)
         print("}")
 
 class OutputHandler_autofocus(OutputHandler):
@@ -98,32 +98,32 @@ class OutputHandler_autofocus(OutputHandler):
         self.rule_enc = ''.join(chr(c) if chr(c).isupper() or chr(c).islower() or chr(c).isdigit() else '_' for c in range(256))
 
     def print_match(self, fpath, page, name, match):
-        string_value = match.replace('hxxp', 'http').replace('\\', '\\\\')
+        string_value = match.replace('hxxp', 'http').replace('//', '////')
 
         if name == "MD5":
-            auto_focus_query = '{"field":"sample.md5","operator":"is","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.md5","operator":"is","value":/"%s/"},' % (string_value)
         elif name == "SHA1":
-            auto_focus_query = '{"field":"sample.sha1","operator":"is","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.sha1","operator":"is","value":/"%s/"},' % (string_value)
         elif name == "SHA256":
-            auto_focus_query = '{"field":"sample.sha256","operator":"is","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.sha256","operator":"is","value":/"%s/"},' % (string_value)
         elif name == "URL":
-            auto_focus_query = '{"field":"sample.tasks.connection","operator":"contains","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.tasks.connection","operator":"contains","value":/"%s/"},' % (string_value)
         elif name == "Host":
-            auto_focus_query = '{"field":"sample.tasks.dns","operator":"contains","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.tasks.dns","operator":"contains","value":/"%s/"},' % (string_value)
         elif name == "Registry":
-            #auto_focus_query = '{"field":"sample.tasks.registry","operator":"is","value":\"%s\"},' % (string_value)
+            #auto_focus_query = '{"field":"sample.tasks.registry","operator":"is","value":/"%s/"},' % (string_value)
             return
         elif name == "Filepath":
-            #auto_focus_query = '{"field":"sample.tasks.file","operator":"is","value":\"%s\"},' % (string_value)
+            #auto_focus_query = '{"field":"sample.tasks.file","operator":"is","value":/"%s/"},' % (string_value)
             return
         elif name == "Filename":
-            #auto_focus_query = '{"field":"alias.filename","operator":"is","value":\"%s\"},' % (string_value)
+            #auto_focus_query = '{"field":"alias.filename","operator":"is","value":/"%s/"},' % (string_value)
             return
         elif name == "Email":
-            #auto_focus_query = '{"field":"alias.email","operator":"is","value":\"%s\"},' % (string_value)
+            #auto_focus_query = '{"field":"alias.email","operator":"is","value":/"%s/"},' % (string_value)
             return
         elif name == "IP":
-            auto_focus_query = '{"field":"sample.tasks.connection","operator":"contains","value":\"%s\"},' % (string_value)
+            auto_focus_query = '{"field":"sample.tasks.connection","operator":"contains","value":/"%s/"},' % (string_value)
         elif name == "CVE":
             return
         print(auto_focus_query) 
@@ -137,7 +137,7 @@ class OutputHandler_autofocus(OutputHandler):
 
     def print_footer(self, fpath):
         rule_name = os.path.splitext(os.path.basename(fpath))[0].translate(self.rule_enc)
-        print('{"field":"sample.tag","operator":"is in the list","value":[\"%s\"]}]}' % (rule_name))
+        print('{"field":"sample.tag","operator":"is in the list","value":[/"%s/"]}]}' % (rule_name))
 
 
 
